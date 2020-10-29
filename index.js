edf564037048eacffa87b0930d77ddb806dface7
Grant Marsh <invee123@gmail.com>
2020-10-29 17:30:20 -0600

    weekly interval in index.js -> Searches trump trends

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const googleTrends = require('google-trends-api');
 
-googleTrends.interestOverTime({keyword: 'Women\'s march'})
+googleTrends.interestOverTime({keyword: 'Trump', startTime: new Date('2017-02-01'), granularTimeResolution: true})
 .then(function(results){
   console.log('These results are awesome', results);
 })
