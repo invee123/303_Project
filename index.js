71948d89f0aa57ce0108036d8406e00605ece23e
Grant Marsh <invee123@gmail.com>
2020-11-09 12:18:47 -0700

    output js to text for python read

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const googleTrends = require('google-trends-api');
+const fs = require('fs');
 
 googleTrends.interestOverTime({keyword: 'Trump', startTime: new Date('2017-02-01'), granularTimeResolution: true})
 .then(function(results){
-  console.log('These results are awesome', results);
+  fs.writeFile("output.txt", results, function(err) {
+      if(err) {
+          return console.log(err);
+      }
+      console.log("The file was saved!");
+  }); 
 })
 .catch(function(err){
-  console.error('Oh no there was an error', err);
+  console.error('Couldn\'t read trends', err);
 });
--- index.py
+++ index.py
@@ -1,7 +1,12 @@
 
 from Naked.toolshed.shell import execute_js, muterun_js
+from Naked.toolshed.types import NakedObject
 
-success = execute_js('index.js')
+success = muterun_js('index.js')
+print(dir(success))
+print(type(success))
+
+
 
 if success:
     print(success)
