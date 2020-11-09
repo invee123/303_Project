0c5227cb644db95b3b4e5d39110ebb9a26ca1331
Grant Marsh <invee123@gmail.com>
2020-11-09 14:49:01 -0700

    set up for multiple analytics - need parser

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ googleTrends.interestOverTime({keyword: 'Trump', startTime: new Date('2017-02-01
 .catch(function(err){
   console.error('Couldn\'t read trends', err);
 });
+
+googleTrends.interestOverTime({keyword: 'Raise Taxes', startTime: new Date('2017-02-01'), granularTimeResolution: true})
+.then(function(results){
+  fs.writeFile("trend_tax.txt", results, function(err) {
+      if(err) {
+          return console.log(err);
+      }
+      console.log("The file was saved!");
+  }); 
+})
+.catch(function(err){
+  console.error('Couldn\'t read trends', err);
+});
--- index.py
+++ index.py
@@ -5,29 +5,30 @@ success = muterun_js('index.js')
 #print(dir(success))
 #print(type(success))
 
-trump_f = open("trend_trump.txt", "r")
-trump = trump_f.read()
-
-
 
 if success:
-    print("success")
+    print('Index.js successfully run')
     #Grants solution
 
     #create arrays
     trump_time = []
+    trump_value = []
 
     #read in file
     trump_f = open("trend_trump.txt", "r")
     trump = trump_f.read()
-    #print(type(trump))
+    trump_f.close()
+
+    tax_f = open("trend_tax.txt", "r")
+    tax = tax_f.read()
+    tax_f.close()
 
     #parse necessary data
     #store parsed data in multiple arrays
         #keep indicies the same
     
     #load into ML model
-    
+
 else:
     print("i hate node w/ python")
 
