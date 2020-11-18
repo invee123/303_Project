13a68bbf2d33433314e1e1f0d28b04d141f0d39f
Caleb Edwards <calebedwards@mymail.mines.edu>
2020-11-17 18:28:51 -0700

    Finished parser.py
    Usage is as shown here in this single commit comment:
    parser_obj = TrendParser(file_path_here)
    dataframe = parser_obj.buildDataFrame()
    
    Note:
    Requires libraries json (included in python) and pandas.

--- parser.py
+++ parser.py
@@ -1,20 +1,16 @@
 import json
+import pandas as pd
 
 # Here we set up a little helper class to handle our timelineData object
 class TimelineElement:
     def __init__(self, timelineData, index):
         data = timelineData[index]
-
-        self.hasData = data['hasData'][0]
-        if (self.hasData):
-            self.time = data['time']
-            self.value = data['value'][0] # exists as a list in the json tho, as do the following few
-            self.formattedValue = data['formattedValue'][0]
-            self.formattedAxisTime = data['formattedAxisTime']
+# Example entry showing all rows:
+# -> {'time': '1486252800', 'formattedTime': 'Feb 5 â€“ 11, 2017', 'formattedAxisTime': 'Feb 5, 2017', 'value': [67], 'hasData': [True], 'formattedValue': ['67']}
+        if (data['hasData'][0]):
+            self.entry = {"hasData":data['hasData'][0], "time":data['time'], "formattedAxisTime":data['formattedAxisTime'], "value":data['value'][0], "formattedValue":data['formattedValue'][0]}
         else:
-            self.time, self.value = 0
-            self.formattedAxisTime, self.formattedValue = ""
-
+            self.entry = {"hasData":data['hasData'][0], "time":"no-data", "formattedAxisTime":"no-data", "value":0, "formattedValue":"no-data"}
 
 class TrendParser:
     def __init__(self, fileURI):
@@ -26,28 +22,29 @@ class TrendParser:
         # Extract our useful data
         self.timelineData = json_data['default']['timelineData']
 
-#    def sortDataByTime():
-#        lowestIndex = 0
-#        for i in range(0, len(self.timelineData)):
-#            compareItem TimelineElement(self.timelineData, lowestIndex)
-#            item = TimelineElement(self.timelineData, i)
-#            if (item.time < compareItem.time)
+        self.dataElements = []
 
+        for i in range(0, len(self.timelineData)):
+            self.dataElements.append(TimelineElement(self.timelineData, i))
 
+    def buildDataFrame(self, dataColumns=["time","formattedAxisTime","value","formattedValue"], includeEmptyElements=False):
+        columns = dict()
+        for col in dataColumns:
+            columns[col] = []
+        for elements in self.dataElements:
+            for col in dataColumns:
+                obj = elements.entry[col]
+                if (obj):
+                    if (elements.entry["hasData"]):
+                        columns[col].append(obj)
+                    elif (includeEmptyElements):
+                        columns[col].append(obj)
+                else:
+                    logging.error("Bad dataColumn entry",col,"could not be found in the data.")
+        df = pd.DataFrame.from_dict(columns, orient="index")
+        return df.transpose()
 
-
-
-#debug comment -- print data very nice
-#print(json.dumps(json_data, indent = 4, sort_keys = True))
-
-# Now that the data is loaded into json_data, we can play with it.
-# json_data['default']['timelineData'] is of interest...
-
-#print(json_data['default']['timelineData'][0])
-# -> {'time': '1486252800', 'formattedTime': 'Feb 5 â€“ 11, 2017', 'formattedAxisTime': 'Feb 5, 2017', 'value': [67], 'hasData': [True], 'formattedValue': ['67']}
-
-#print(type(json_data['default']['timelineData'][0]['value']))
-# -> list
-
-#test = TimelineElement(timelineData, 0)
-#print(test.formattedAxisTime)
+########## EXAMPLE RUN ############
+# p = TrendParser("trend_tax.txt")
+# df = p.buildDataFrame()
+# print(df.head())
